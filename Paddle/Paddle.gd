10e1ffac06e641373d0e9f220de09fd6f429ec41
Diamondskeleton0 <iwilliams21212122@gmail.com>
2023-10-03 10:57:42 -0400

    completes exercise 3-1

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -10,12 +10,15 @@ func _ready():
 	target = Vector2(Global.VP.x / 2, Global.VP.y - width)
 
 func _physics_process(_delta):
-	target.x = clamp(target.x, 0, Global.VP.x - 2*width)
+	target.x = clamp(target.x, 0, Global.VP.x)
 	position = target
+	if $Highlight.modulate.a > 0:
+		$Highlight.modulate.a -= decay
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		target.x += event.relative.x
 
 func hit():
-	pass
+	$Highlight.modulate.a = 1.0
+	$Confetti.emitting = true
